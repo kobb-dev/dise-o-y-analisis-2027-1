019faecaa0f7299e0eb85e1709e9526e5addedd6
kobb-dev <kobblery@gmail.com>
2026-09-10 02:20:40 -0600

    Add files via upload

---
+++ Tarea4.py
@@ -0,0 +1,46 @@
+from selectors import SelectSelector
+
+
+def busqBinaria(datos, buscados):
+    noEncontrado = True
+    inicio = 0
+    fin = len(datos) - 1
+
+    while inicio <= fin:
+        numMedio = (inicio + fin) // 2
+
+        if buscados == datos[numMedio]:
+            print(f"El num {buscados} se encopntro en el {numMedio} :D")
+            noEncontrado = False
+            break
+        elif buscados > numMedio:
+            inicio = numMedio +1
+
+        else:
+            fin = numMedio +1
+    if noEncontrado:
+        print("No existe :d")
+
+lim = 10
+info = [x*5 for x in range(1,lim+1)]
+busqBinaria(info, lim*5)
+
+lim = 100
+info = [x*5 for x in range(1,lim+1)]
+busqBinaria(info, lim*5)
+
+lim = 1000
+info = [x*5 for x in range(1,lim+1)]
+busqBinaria(info, lim*5)
+
+lim = 10000
+info = [x*5 for x in range(1,lim+1)]
+busqBinaria(info, lim*5)
+
+lim = 100000
+info = [x*5 for x in range(1,lim+1)]
+busqBinaria(info, lim*5)
+
+lim = 1000000
+info = [x*5 for x in range(1,lim+1)]
+busqBinaria(info, lim*5)
